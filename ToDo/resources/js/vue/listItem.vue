588a108c655244f5db2daef76ac3f996260d65a0
JGasinski <jjgasinski@gmail.com>
2021-11-09 09:35:15 +0100

    fix2

--- ToDo/resources/js/vue/listItem.vue
+++ ToDo/resources/js/vue/listItem.vue
@@ -1,13 +1,67 @@
 <template>
-    <div>
-        List Item
+    <div class="item">
+        <input
+            type="checkbox"
+            @change="updateCheck()"
+            v-model="item.completed"
+            />
+            <span :class="[item.completed ? 'completed' : '', 'item.text']"> {{ item.name }} </span>
+            <button @click="removeItem()" class="trashcan" >
+                <font-awesome-icon icon="trash" />
+            </button>
     </div>
-
 </template>
 
 <script>
 export default {
-
-
+    props: ['item'],
+    methods: {
+        updateCheck() {
+            axios.put('api/item/' + this.item.id, {
+                item: this.item
+            })
+            .then( resonse => {
+                if( resonse.status == 200 ) {
+                    this.$emit('itemchanged');
+                }
+            })
+            .catch( error => {
+                console.log( error );
+            })
+        },
+        removeItem() {
+            axios.delete('api/item/' + this.item.id )
+            .then( response => {
+                if( response.status == 200 ) {
+                    this.$emit('itemchanged');
+                }
+            })
+            .catch ( error => {
+                console.log( error );
+            })
+        }
+    }
 }
 </script>
+
+<style scoped>
+.completed {
+    text-decoration: line-through;
+    color: rgb(247, 10, 10);
+}
+.itemText {
+    width: 100%;
+    margin-left: 20px;
+}
+.item {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+.trashcan {
+    background: rgb(217, 243, 217);
+    border: none;
+    color: rgb(218, 45, 45);
+    outline: none;
+}
+</style>
--- ToDo/resources/js/vue/listView.vue
+++ ToDo/resources/js/vue/listView.vue
@@ -1,12 +1,34 @@
 <template>
-    <div>
-        List View
+    <div>      
+        <div v-for="(item, index) in items" :key="index" >
+            <list-item
+                :item="item"
+                class="item"
+                v-on:itemchanged="$emit('reloadlist')"
+                />
+        </div>
     </div>
 </template>
 
-<script>
+<script> 
+
+import listItem from "./listItem"
+//import listItem from './listItem.vue'
+
 export default {
-
-
+    props: [ 'items'],
+    components: {
+        listItem
+        
+    }
 }
 </script>
+
+<style scoped>
+.item {
+
+    background: rgb(217, 243, 217);
+    padding: 5px;
+    margin-top: 5px;
+}
+</style>
